9124b5cc73bb1b0bd60ce9f07d562fefd1af9712
AbdifatahYasin1 <iskatag123@gmail.com>
2022-12-10 03:52:02 +0300

    updated

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import "./style.css";
-const input = document.querySelector("#input");
-const form = document.querySelector("#form");
-const todos = document.querySelector("#todo-list");
-const clearbtn = document.querySelector(".clear-btn");
+import './style.css';
 
-const createElement = (tag, className, dataset = "", text = "") => {
+const input = document.querySelector('#input');
+const form = document.querySelector('#form');
+const todos = document.querySelector('#todo-list');
+const clearbtn = document.querySelector('.clear-btn');
+
+const createElement = (tag, className, dataset = '', text = '') => {
   const element = document.createElement(tag);
   element.className = className;
-  className === "check-btn" && element.setAttribute("type", "checkbox");
+  className = 'check-btn' && element.setAttribute('type', 'checkbox');
   element.dataset.id = dataset;
   element.textContent = text;
   return element;
@@ -17,107 +18,105 @@ const reArrange = (list) => {
   if (!Array.isArray(list)) {
     return;
   }
-  let newList = [];
+  const newList = [];
   list.forEach((item, index) => {
     newList.push({ id: index, text: item.text, completed: item.completed });
   });
-  localStorage.setItem("list", JSON.stringify(newList));
+  localStorage.setItem('list', JSON.stringify(newList));
 };
 
 const Save = (text) => {
   let list = [];
-  if (localStorage.getItem("list")) {
-    list = JSON.parse(localStorage.getItem("list"));
+  if (localStorage.getItem('list')) {
+    list = JSON.parse(localStorage.getItem('list'));
   }
-  list.push({ id: list.length, text: text, completed: false });
-  localStorage.setItem("list", JSON.stringify(list));
+  list.push({ id: list.length, text, completed: false });
+  localStorage.setItem('list', JSON.stringify(list));
   return list;
 };
 
 const checkTodo = (e) => {
-  const id = e.target.dataset.id;
-  const list = JSON.parse(localStorage.getItem("list"));
-  const item = list.find((item) => item.id == id);
+  const { id } = e.target.dataset;
+  const list = JSON.parse(localStorage.getItem('list'));
+  const item = list.find((item) => item.id === id);
   item.completed = !item.completed;
-  localStorage.setItem("list", JSON.stringify(list));
+  localStorage.setItem('list', JSON.stringify(list));
   reArrange(todos);
-  select = document.querySelector(`[data-id="${id}"]`);
-  select.classList.toggle("completed");
-};
-
-const addTodo = (e) => {
-  e.preventDefault();
-  if (input.value === "") {
-    input.classList.add("error");
-    input.classList.add("shake");
-    setTimeout(() => {
-      input.classList.remove("error");
-      input.classList.remove("shake");
-    }, 1000);
-
-    return;
-    return;
-  }
-  Save(input.value);
-  loadTodos();
-  input.value = "";
-};
-
-const deleteTodo = (e) => {
-  //delete from local storage if delete button is clicked
-  const id = e.target.dataset.id;
-  const list = JSON.parse(localStorage.getItem("list"));
-  list.splice(id, 1);
-  localStorage.setItem("list", JSON.stringify(list));
-  loadTodos();
+  const select = document.querySelector(`[data-id="${id}"]`);
+  select.classList.toggle('completed');
 };
 
 const loadTodos = () => {
-  todos.innerHTML = "";
-  if (localStorage.getItem("list")) {
-    const list = JSON.parse(localStorage.getItem("list"));
+  const deleteTodo = (e) => {
+    // delete from local storage if delete button is clicked
+    const { id } = e.target.dataset;
+    const list = JSON.parse(localStorage.getItem('list'));
+    list.splice(id, 1);
+    localStorage.setItem('list', JSON.stringify(list));
+    loadTodos();
+  };
+  todos.innerHTML = '';
+  if (localStorage.getItem('list')) {
+    const list = JSON.parse(localStorage.getItem('list'));
     reArrange(list);
-    const newList = JSON.parse(localStorage.getItem("list"));
+    const newList = JSON.parse(localStorage.getItem('list'));
 
     newList.forEach((item) => {
-      const todo = createElement("li", "todo", item.id, "");
+      const todo = createElement('li', 'todo', item.id, '');
 
-      const checkBox = createElement("input", "check-btn", item.id, "");
-      checkBox.addEventListener("change", (e) => checkTodo(e));
+      const checkBox = createElement('input', 'check-btn', item.id, '');
+      checkBox.addEventListener('change', (e) => checkTodo(e));
       todo.appendChild(checkBox);
-      const text = createElement("span", "todo-text", item.id, item.text);
+      const text = createElement('span', 'todo-text', item.id, item.text);
       todo.appendChild(text);
-      const deleteBtn = createElement("i", "fa fa-trash", item.id, "");
-      deleteBtn.addEventListener("click", (e) => deleteTodo(e));
+      const deleteBtn = createElement('i', 'fa fa-trash', item.id, '');
+      deleteBtn.addEventListener('click', (e) => deleteTodo(e));
       todo.appendChild(deleteBtn);
 
       todos.appendChild(todo);
       if (item.completed) {
-        todo.classList.add("completed");
+        todo.classList.add('completed');
         checkBox.checked = true;
       }
     });
   }
 };
 
-//clear all todos
-clearbtn.addEventListener("click", () => {
-  if (!localStorage.getItem("list")) {
+const addTodo = (e) => {
+  e.preventDefault();
+  if (input.value === '') {
+    input.classList.add('error');
+    input.classList.add('shake');
+    setTimeout(() => {
+      input.classList.remove('error');
+      input.classList.remove('shake');
+    }, 1000);
+
     return;
   }
-  let list = JSON.parse(localStorage.getItem("list"));
+  Save(input.value);
+  loadTodos();
+  input.value = '';
+};
+
+// clear all todos
+clearbtn.addEventListener('click', () => {
+  if (!localStorage.getItem('list')) {
+    return;
+  }
+  const list = JSON.parse(localStorage.getItem('list'));
   const filteredList = list.filter((item) => item.completed === false);
-  localStorage.setItem("list", JSON.stringify(filteredList));
+  localStorage.setItem('list', JSON.stringify(filteredList));
   loadTodos();
 });
 
-form.addEventListener("submit", (e) => addTodo(e));
+form.addEventListener('submit', (e) => addTodo(e));
 
-document.querySelector(".reload").addEventListener("click", () => loadTodos());
+document.querySelector('.reload').addEventListener('click', () => loadTodos());
 
 const init = () => {
-  console.log("App is running");
+  console.log('App is running');
   loadTodos();
 };
 
-document.addEventListener("DOMContentLoaded", init);
+document.addEventListener('DOMContentLoaded', init);
