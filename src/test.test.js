7a2d9160dbac7d421cea5301e73273f33c45328e
bernabasy <104406349+bernabasy@users.noreply.github.com>
2022-12-23 09:00:41 +0300

    Update edittodo test

--- src/test.test.js
+++ src/test.test.js
@@ -20,15 +20,6 @@ describe('Testing Todos function', () => {
     array = TodoFunction.deleteTodo(1, array);
     expect(array).toHaveLength(2);
   });
-
-  test('Clear All test function', () => {
-    array = [
-      { id: 0, text: 'test', completed: false },
-      { id: 1, text: 'test2', completed: true },
-      { id: 1, text: 'test2', completed: false },
-    ];
-    expect(TodoFunction.clearAll(array)).toHaveLength(2);
-  });
   test('should edit todo taskes', () => {
     array = [
       { id: 0, text: 'test', completed: false },
@@ -39,6 +30,15 @@ describe('Testing Todos function', () => {
     // change text tobe changed
     expect(array.find((item) => item.id === 0).text).toBe('changed');
   });
+  test('Clear All test function', () => {
+    array = [
+      { id: 0, text: 'test', completed: false },
+      { id: 1, text: 'test2', completed: true },
+      { id: 1, text: 'test2', completed: false },
+    ];
+    expect(TodoFunction.clearAll(array)).toHaveLength(2);
+  });
+
   test('should Check todo taskes', () => {
     array = [
       { id: 0, text: 'test', completed: false },
