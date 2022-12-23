64335d07afff629b6d39fe81a966f3a2935a2af8
bernabasy <104406349+bernabasy@users.noreply.github.com>
2022-12-23 08:58:29 +0300

    Update teast case

--- src/test.test.js
+++ src/test.test.js
@@ -1,8 +1,8 @@
 const TodoFunction = require('./practice');
 
 let array = [];
-describe('Testing Todos', () => {
-  test('add test', () => {
+describe('Testing Todos function', () => {
+  test('should add test', () => {
     expect(TodoFunction.addTodo('', array)).toHaveLength(0);
     array = TodoFunction.addTodo('test1', array);
     expect(array).toHaveLength(1);
@@ -10,7 +10,7 @@ describe('Testing Todos', () => {
     expect(array).toHaveLength(2);
   });
 
-  test('delete test', () => {
+  test('delete test function', () => {
     array = [
       { id: 0, text: 'test', completed: false },
       { id: 1, text: 'test2', completed: false },
@@ -21,7 +21,7 @@ describe('Testing Todos', () => {
     expect(array).toHaveLength(2);
   });
 
-  test('Clear All test', () => {
+  test('Clear All test function', () => {
     array = [
       { id: 0, text: 'test', completed: false },
       { id: 1, text: 'test2', completed: true },
@@ -29,7 +29,7 @@ describe('Testing Todos', () => {
     ];
     expect(TodoFunction.clearAll(array)).toHaveLength(2);
   });
-  test('should edit todo', () => {
+  test('should edit todo taskes', () => {
     array = [
       { id: 0, text: 'test', completed: false },
       { id: 1, text: 'test2', completed: false },
@@ -39,7 +39,7 @@ describe('Testing Todos', () => {
     // change text tobe changed
     expect(array.find((item) => item.id === 0).text).toBe('changed');
   });
-  test('should Check todo', () => {
+  test('should Check todo taskes', () => {
     array = [
       { id: 0, text: 'test', completed: false },
       { id: 1, text: 'test2', completed: false },
